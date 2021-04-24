920379fc54ed4a773a324d83ec1ba35e7d7f8dca
Juan Pablo Brussini <jpbrusini1@gmail.com>
2021-04-24 20:02:34 -0300

    push

--- js/funciones.js
+++ js/funciones.js
@@ -1,158 +1,131 @@
-console.log('desde funciones')
+//Activar el formulario
+let formEqui = document.getElementById('formEqui') 
+formEqui = addEventListener('submit', pointEq) 
 
-//Razon Social y Provincia de la Empresa
-function Empresas (razonSocial, provincia) {
-  this.razonSocial = prompt('Razon Social').toUpperCase();
-  this.provincia = prompt('Provincia').toUpperCase();
-  
-  }
-const companias  = new Empresas('Razon Social', 'Provincia' )
-console.log(companias)
 
-//Marca y Rubro de las Pymes
-class Pymes {
-constructor(marca, rubro) {
-    this.marca = prompt('Marca').toUpperCase();
-    this.rubro = prompt('Rubro').toUpperCase();
+//funcion para obtener los valores
+function valores() {
+
+let marcas = document.getElementById('marca').value;
+let rubros = document.getElementById('rubro').value;;
+let alquiler = document.getElementById('alquileres').value;
+let servicios = document.getElementById('servicio').value;
+let sueldos = document.getElementById('sueldo').value;
+let honorariosProf = document.getElementById('honorariosPro').value;
+let honorariosGer = document.getElementById('honorariosGe').value;
+let precioVenta = document.getElementById('preciovent').value; 
+let comisiones = document.getElementById('comisione').value;
+let cmv = 50 / 100;
+let iibb = 4 / 100;
+let gcias = 5 /100;
+let gsadm = 2 / 100;
+let gsvs = 2 / 100;  
+let percent = 100;
+
+return {marcas, rubros, alquiler, servicios, sueldos, honorariosProf, honorariosGer, precioVenta, comisiones, cmv, iibb, gcias, gsadm, gsvs, percent}
+
 }
-}
 
-const pequeñasEmpresas = []
-
-//fn para obterner todas las marcas y rubros
-
-const getAll = () => {
-return pequeñasEmpresas
-}
-const create = (pyme) => { 
-pequeñasEmpresas.push(pyme)
-}
-const pyme1 = new Pymes ('Marca', 'Rubro')
-create(pyme1)
-//const pyme2 = new Pymes ('Marca', 'Rubro')
-//create(pyme2)
-
-console.log(getAll())
 
 //funcion para calcular el punto de equilibrio
-function point () {
+function pointEq(event) {
+  event.preventDefault();
 
-//funcio para calcular los costos fijos mensuales
-function cfm (){ 
-
-let alquiler = parseInt (prompt('Alquiler Mensual $'));
-let servicios = parseInt (prompt('Servicios Mensuales (Luz,Internet,etc) $'));
-let sueldos = parseInt (prompt('Sueldos Brutos Mensuales $'));
-let honorariosProf = parseInt (prompt('Honorarios Prof Mensuales $'));
-let honorariosGer = parseInt (prompt('Honorarios Gerenciales Mensuales $'));
-
-
-const arrayDatosIngresadosFijos = [ alquiler, servicios, sueldos, honorariosProf, honorariosGer];
-console.log( arrayDatosIngresadosFijos.length );
-//es posible que mas adelante utilice estos datos para trabajar en optimizar el simulador agregandole la inflacion
-const masInflacionMensual   = arrayDatosIngresadosFijos.map(x => x * 1.03);
-console.log( masInflacionMensual );
-
-function suma(alquiler, servicios, sueldos, honorariosProf, honorariosGer) {
-   return alquiler + servicios + sueldos + honorariosProf + honorariosGer
-}
-let costosFijos = suma(alquiler,servicios,sueldos,honorariosProf,honorariosGer);
-alert('Los costos fijos mensuales son $ ' + costosFijos.toLocaleString("es-ES"));
-
-//funcion para calcular los costos variables por unidad
-function cvu () {
-  let precioVenta = parseInt (prompt('Precio de Venta Unitario Promedio $'));   
-  let cmv = 50 / 100;
-  let comisiones = parseInt (prompt('Tasa de comisiones por venta % (0 al 100)'));
-  let iibb = 4 / 100;
-  let gcias = 5 /100;
-  let gsadm = 2 / 100;
-  let gsvs = 2 / 100;  
-  let percent = 100;
-
-//luego es posible que muestre estos valores en el HTML
-const arraySupuestosPymes = [ cmv, iibb, gcias, gsadm, gsvs];
-console.log( arraySupuestosPymes.length );
-const porCien   = arraySupuestosPymes.map(x => x * 100);
-console.log( porCien );
-
-//luego es posible que utilice estos datos
-const arrayDatosIngresadosVariables = [ precioVenta, comisiones];
-console.log( arrayDatosIngresadosVariables.length );
-
-
-//funcion para obtener el costo variable unitario
-  function mas (precioVenta, cmv, comisiones, iibb, gcias, gsadm, gsvs ) {
+const {marcas, rubros, alquiler, servicios, sueldos, honorariosProf, honorariosGer, precioVenta, comisiones, cmv, iibb, gcias, gsadm, gsvs, percent} = valores();
+//obtencion de costos fijos
+let costosFijos = parseInt(alquiler) + parseInt(servicios) + parseInt(sueldos) + parseInt(honorariosProf) + parseInt(honorariosGer);
+function mas (precioVenta, cmv, comisiones, iibb, gcias, gsadm, gsvs ) {
   return precioVenta * cmv + precioVenta * comisiones / percent + precioVenta * iibb + precioVenta * gcias + precioVenta * gsadm + precioVenta * gsvs
 
   }
+  //obtencion de costos variables
   let costoVariable = mas(precioVenta, cmv, comisiones, iibb, gcias, gsadm, gsvs );
-  alert('Los costos variables por producto son $' + costoVariable.toLocaleString("es-ES"));
 
-  //funcion para obtener el margen bruto unitario
-  function margenu () {
-  
-    function maru (precioVenta, costoVariable ) {
+  function maru (precioVenta, costoVariable ) {
     return precioVenta - costoVariable
   
     }
+    //obtencion de margen unitario
     let margenUnitario = maru(precioVenta, costoVariable );
-    //alert('El margen bruto unitario es ' + margenUnitario);
-  
-  //calculo del punto de equilibrio mensual en unidades
-  function pointEq () {
-  
+
     function equi (costosFijos, margenUnitario ) {
-    return costosFijos / margenUnitario
-  
-    }
-    let puntoEqUni = parseInt (equi(costosFijos, margenUnitario ));
-    alert('El punto de equilibrio de ventas mensuales en unidades es ' + puntoEqUni);
-  
-
-  //calculo del punto de equilibrio mensual en ingresos $
-
-    function pointVentasmes () {
-  
-      function equil (puntoEqUni, precioVenta ) {
-      return puntoEqUni * precioVenta
+      return costosFijos / margenUnitario
     
       }
-      let puntoEqMes = parseInt (equil (puntoEqUni, precioVenta ));
-    
-      alert('El punto de equilibrio de ventas mensuales es $ ' + puntoEqMes.toLocaleString("es-ES"));
+      //obtencion de punto de Equilibrio Unitario mensual
+      let puntoEqUni = parseInt (equi(costosFijos, margenUnitario ));
   
-
-//calculo del punto de equilibrio anual en ingresos $
-  function pointVentasanual () {
+      function equil (puntoEqUni, precioVenta ) {
+        return puntoEqUni * precioVenta
+      
+        }
+        //obtencion de punto de Equilibrio de Ventas en $  mensuales
+        let puntoEqMes = parseInt (equil (puntoEqUni, precioVenta ));
   
-    let meses = 12
-    function equili (puntoEqMes, meses ) {
-    return puntoEqMes * meses
-  
-    }
-    let puntoEqAn = parseInt (equili (puntoEqMes, meses ));
-  
-    alert('El punto de equilibrio de ventas anuales es $ ' + puntoEqAn.toLocaleString("es-ES"));
+        let meses = 12
+
+        
+        //obtencion de punto de Equilibrio de Ventas en $  anuales
+        function equili (puntoEqMes, meses ) {
+        return puntoEqMes * meses
+              }
+        let puntoEqAn = parseInt (equili (puntoEqMes, meses ));
+
+     
+        //Almacenar Marca y Rubro para luego mostrar en resultado
+      localStorage.setItem('marcas', marcas)
+      localStorage.setItem('rubros', rubros)
+      let marcaLocal = localStorage.getItem('marcas')
+      let rubroLocal = localStorage.getItem('rubros')
+        
+
+resulting(marcaLocal, rubroLocal, costoVariable, costosFijos, puntoEqAn, puntoEqMes, puntoEqUni)
 
 
 }
+//Funcion para insertar en HTML los resultados obtenidos en el simulador
+function resulting(marcaLocal, rubroLocal, costoVariable, costosFijos, puntoEqAn, puntoEqMes, puntoEqUni) {
+let result = document.getElementById(`result`)
+let print = document.createElement(`div`)
 
-pointVentasanual()
-    }
+print.innerHTML = `
 
-    pointVentasmes()
+<div class="container-data row">
+<div class="col s4">
+<h5 class=puntounid>Punto de Equilibrio Ventas Unitario Mes es ${puntoEqUni.toLocaleString("es-ES")} unidades</h5>
+</div>
+<div class="col s4">
+<h5 class=puntovtam>Punto de Equilibrio Ventas por Mes es $ ${puntoEqMes.toLocaleString("es-ES")}</h6>
+</div>  
+<div class="col s4">
+<h5 class=puntovtaa>Punto de Equilibrio Ventas   Anuales   es     $  ${puntoEqAn.toLocaleString("es-ES")}</h5>
+</div>
 
+</div>
+<div>
+<h6 class=marca>Marca  ${marcaLocal}<h6>
+<h6 class=rubro>Rubro  ${rubroLocal}<h6>
+</div>
+<div>
+
+<img src="multimedia/puntodeeq.jpg" class="img1" alt="punto de equilibrio">
+
+</div>
+`  
+  result.appendChild(print)
+  reset();
 
 }
-pointEq()
 
-  }
-margenu()
-}
-cvu()
-}
-cfm()
-}
+//Funcion para resetear los datos ingresados en el formulario
+function reset() {
+document.getElementById('formEqui').reset()  
+} 
 
-point()
+
+
+
+
+
+
+
